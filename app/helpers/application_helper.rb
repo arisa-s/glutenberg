8baffd62679335075e88fd2f5a43856c2f600c01
arisas <arisashiraishi@outlook.com>
2026-03-02 18:04:25 +0100

    add link to ingredient highlight

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,12 +5,17 @@ module ApplicationHelper
     Rails.env.production?
   end
 
-  def highlight_product(original_string, product)
+  def highlight_product(original_string, product, link: false)
     return original_string if original_string.blank? || product.blank?
 
     escaped = Regexp.escape(product)
     highlighted = original_string.sub(/(#{escaped})/i) do
-      content_tag(:span, Regexp.last_match(1), class: "product-highlight")
+      span = content_tag(:span, Regexp.last_match(1), class: "product-highlight")
+      if link
+        link_to(span, ingredients_path(product: product), class: "product-highlight-link")
+      else
+        span
+      end
     end
 
     highlighted.html_safe
